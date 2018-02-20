88edc4f3f0fc130ea6de45186a713011c52f4611
isanmartin <isanmartin@keedio.com>
2018-02-20 12:12:16 +0100

    Modified error message

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -256,7 +256,7 @@ def runGenericJenkinsfile() {
                 if (!isValidVersion) {
                     //Sufix -SNAPSHOT is required for develop and feature branch types and is forbidden for release,hotfix and master branch types
                     currentBuild.result = 'FAILURE'
-                    throw new hudson.AbortException('Version of artifact in pom is not allowed to this type of branch')
+                    throw new hudson.AbortException('Version of artifact in pom is not allowed for this type of branch')
                 }
 
             }
