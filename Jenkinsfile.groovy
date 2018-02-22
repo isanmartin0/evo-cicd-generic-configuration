ca41722bb8505c032d970d67adf7b2a5a94bc53f
isanmartin <isanmartin@keedio.com>
2018-02-22 17:23:07 +0100

    Modify method for remove LogRotator properties

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -537,6 +537,9 @@ def runGenericJenkinsfile() {
 
             } else {
                 echo "Not removing old builds."
+
+                properties([[$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '']]]);
+
             }
 
         }
